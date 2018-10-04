9dd134b26cb387e86f7cdcb680f401f564fd24b3
ARJUNA <ARJUNA@LAPTOP-0HAV1RQA>
2018-10-04 20:53:35 +0700

    commit keseluruhan

---
+++ src/Modul4/hewan.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modul4;
+
+/**
+ *
+ * @author ARJUNA
+ */
+public class hewan {
+    void setNama() {
+      String hewan = "Tidak Bernama";
+      System.out.println("Nama hewan: "+hewan);
+    }
+    void setSuara() {
+      String suara = "Tidak Bernama";
+      System.out.println("Suara hewan: "+suara);
+    }
+    void setBerat() {
+      double berat = 0.0;
+      System.out.println("berat Hewan: "+berat+" Kg");
+    }
+    
+}
